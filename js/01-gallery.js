6ff5a54b832970b1360418233d894ed20044c450
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-09-19 17:05:14 +0300

    complete first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const refs = {
+  renderedGalleryList: document.querySelector(".gallery"),
+};
+
+// =========================================================
+
+
+function imgTemplate (el) {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>
+</li>`
+}
+
+
+function renderGalleryElFromTemplate() {
+  const markup = galleryItems.map(imgTemplate).join("");
+  refs.renderedGalleryList.innerHTML = markup;
+}
+
+renderGalleryElFromTemplate();
+
+// ========================================================
+
+refs.renderedGalleryList.addEventListener("click", onElClick);
+
+
+function onElClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return
+  };
+
+  event.preventDefault();
+  // console.log(event.target.dataset.source);
+
+  const instance = basicLightbox.create(`
+  <div class="modal">
+    <img src="${event.target.dataset.source}" width="800" height="600">
+  </div>
+  `, {
+	closable: true
+  })
+
+  instance.show()
+
+};
+
+// ==========================================================
